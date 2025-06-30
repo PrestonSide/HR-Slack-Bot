cdadbda74f258cd3b8847878aa24a2cf426d922d
Preston Sidenstricker <preston.sidenstricker@metrocomm.com>
2025-06-30 14:36:53 -0500

    Test Remove Imports

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ from slack_sdk.errors import SlackApiError
 from slack_sdk.signature import SignatureVerifier
 
 ## LLaMa 3 Imports ##
-from langchain.chains import RetrievalQA
-from langchain_community.vectorstores import FAISS
-from langchain.text_splitter import CharacterTextSplitter
+# from langchain.chains import RetrievalQA
+# from langchain_community.vectorstores import FAISS
+# from langchain.text_splitter import CharacterTextSplitter
 from langchain_openai import ChatOpenAI
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_huggingface import HuggingFaceEmbeddings
+# from langchain_community.document_loaders import PyPDFLoader
+# from langchain_huggingface import HuggingFaceEmbeddings
 
 
 import os
@@ -40,25 +40,25 @@ verifier = SignatureVerifier(signing_secret=signing_secret)
 ## Doc Retrieval ##
 
 
-loader1 = PyPDFLoader("handbook.pdf")
-loader2 = PyPDFLoader("retirement.pdf")
+# loader1 = PyPDFLoader("handbook.pdf")
+# loader2 = PyPDFLoader("retirement.pdf")
 
-docs1 = loader1.load()
-docs2 = loader2.load()
-docs = docs1 + docs2
+# docs1 = loader1.load()
+# docs2 = loader2.load()
+# docs = docs1 + docs2
 
-splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=50)
-texts = splitter.split_documents(docs)
+# splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+# texts = splitter.split_documents(docs)
 
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-vectorstore = FAISS.from_documents(texts, embeddings)
+# embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+# vectorstore = FAISS.from_documents(texts, embeddings)
 
 llm = ChatOpenAI(
         base_url="https://openrouter.ai/api/v1", 
         openai_api_key= openrouter_key,  # from openrouter.ai/keys
         model="mistralai/mistral-7b-instruct" # or "meta-llama/llama-3-70b-instruct", etc.
     )
-qa_chain = RetrievalQA.from_chain_type(llm=llm, retriever=vectorstore.as_retriever(), return_source_documents=False)
+#a_chain = RetrievalQA.from_chain_type(llm=llm, retriever=vectorstore.as_retriever(), return_source_documents=False)
 
 
 
@@ -74,10 +74,10 @@ def ask_llama():
     if not user_input:
         return jsonify({"error": "Missing 'question' in request"}), 400
 
-    answer = qa_chain.run(user_input)
+    answer = llm.invoke(user_input)
     print(answer)
     #debug_log(f"AI Answer: {answer}")
-    return answer #jsonify({"answer": str(answer.content if hasattr(answer, "content") else answer)})
+    return jsonify({"answer": str(answer.content if hasattr(answer, "content") else answer)})
 
 ## Post method responses ##
 @app.route("/slack/events", methods=["POST"])
